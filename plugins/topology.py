625a2adb9e530d4841eff0a4d905f21d28eff4a6
Shubhransh Jagota <shubhransh.jagota@phonepe.com>
2024-05-08 15:24:10 +0530

    remove the commented code

--- plugins/topology.py
+++ plugins/topology.py
@@ -4,12 +4,10 @@ from collections import OrderedDict
 
 import epochclient
 import plugins
-import tenacity
 
-from operator import itemgetter
 import epochutils
-from tenacity import retry
 from types import SimpleNamespace
+
 
 class Applications(plugins.EpochPlugin):
     def __init__(self) -> None:
@@ -20,11 +18,6 @@ class Applications(plugins.EpochPlugin):
 
         commands = parser.add_subparsers(help="Available commands for topology management")
 
-        # sub_parser = commands.add_parser("list", help="List all topology")
-        # sub_parser.add_argument("--sort", "-s", help="Sort output by column", type=int, choices=range(0, 9), default = 0)
-        # sub_parser.add_argument("--reverse", "-r", help="Sort in reverse order", action="store_true")
-        # sub_parser.set_defaults(func=self.list_apps)
-        #
         sub_parser = commands.add_parser("list", help="Show all the topologies on the cluster")
         sub_parser.set_defaults(func=self.list)
 
@@ -46,7 +39,6 @@ class Applications(plugins.EpochPlugin):
     def get_runs(self, options: SimpleNamespace):
         data = self.epoch_client.get("/apis/v1/topologies/{topology_id}/runs".format(topology_id=options.topology_id))
         epochutils.print_json(data)
-
 
     def get(self, options: SimpleNamespace):
         data = self.epoch_client.get("/apis/v1/topologies/{topology_id}".format(topology_id=options.topology_id))
